30a5f8028218519920702acdc965fab6b6e556da
maybetaken <jiangyabin8@gmail.com>
2022-04-01 13:52:35 +0800

    init

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <spdlog/spdlog.h>
+
+int main(){
+   std::cout << "Hello world!" << std::endl;
+   spdlog::info("hello spdlog");
+   return 0;
+}
---
+++ tests/examples/main.cpp
@@ -0,0 +1,7 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest/doctest.h"
+
+TEST_CASE("TEST DEMO"){
+    CHECK(1 == 1);
+    CHECK(1 == 1);
+}
